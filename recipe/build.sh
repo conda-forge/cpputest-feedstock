a4cd21d9f9a90e4acdf56a1b5c18e519dfcb80a1
Tyler Hoffman <tyler@memfault.com>
2020-07-25 13:37:09 -0700

    Add activate scripts to set CPPUTEST_HOME

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
+
 mkdir -p build
 cmake -S . -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX
 cmake --build build --target install
