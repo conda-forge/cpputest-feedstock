f0cacf359c8cf8d099ede6ce4d92690abaabfc44
Daniel Nachun <daniel.nachun@gmail.com>
2025-05-23 08:27:55 -0700

    enable platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,38 @@ do
     cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 done
 
+if [[ ${CONDA_BUILD_CROSS_COMPILATION:-0} == 1 ]]; then
+    BOOTSTRAP_CMAKE_ARGS=${CMAKE_ARGS//${PREFIX}/${BUILD_PREFIX}}
+    BOOTSTRAP_CMAKE_ARGS=${BOOTSTRAP_CMAKE_ARGS//${CONDA_TOOLCHAIN_HOST}/${CONDA_TOOLCHAIN_BUILD}}
+
+    CROSS_LDFLAGS=${LDFLAGS}
+    CROSS_CC="${CC}"
+    CROSS_CXX="${CXX}"
+    CROSS_LD="${LD}"
+
+    LDFLAGS=${LDFLAGS//${PREFIX}/${BUILD_PREFIX}}
+    CC=${CC_FOR_BUILD}
+    CC=${CXX_FOR_BUILD}
+    unset LD
+
+    cmake -S . -B build_host \
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_VERBOSE_MAKEFILE=ON \
+        -Wno-dev \
+        -DBUILD_TESTING=OFF \
+        ${BOOTSTRAP_CMAKE_ARGS}
+    cmake --build build_host -j${CPU_COUNT}
+    cmake --install build_host
+
+    LDFLAGS="${CROSS_LDFLAGS}"
+    CC=${CROSS_CC}
+    CXX=${CROSS_CXX}
+    LD=${CROSS_LD}
+
+    sed -i -e "s,\$<TARGET_FILE:\${EXECUTABLE}>,$SRC_DIR/build_host/tests/CppUTest/CppUTestTests,g" src/cmake/modules/CppUTestBuildTimeDiscoverTests.cmake
+fi
+
 cmake -S . -B build \
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
     -DCMAKE_BUILD_TYPE=Release \
